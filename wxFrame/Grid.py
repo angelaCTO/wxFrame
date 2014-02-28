14b66eb98be2c6c4435682200809f2c7c6a56c89
Angela To <to.angelac@gmail.com>
2014-02-28 07:24:57 +0100

    Added Grid test functions

--- wxFrame/Grid.py
+++ wxFrame/Grid.py
@@ -1,4 +1,4 @@
-# author: Samuel Chen
+import random
 
 class Grid:
     def __init__(self, r, c):
@@ -8,12 +8,37 @@ class Grid:
         self.buffer = [0] * self.length
         self.maxSet = False
         self.avgSet = False
+        self.counter = 0
 
-    #
+    # Sets grid element values to alterate between 0 and 1 
+    # for testing
+    def toggleGridTest(self):
+        if self.counter == 0 :
+            # Set alternating grid
+            for y in range (self.column):
+                for x in range (self.row):
+                    if (x % 2 == 0 and y % 2 == 0):
+                        self.setValue(x,y,0)
+            self.counter = 1
+        else :
+            for y in range (self.column):
+                for x in range (self.row):
+                    if (x % 2 == 0 and y % 2 == 0):
+                        self.setValue(x,y,1)
+            self.counter = 0
+
+    # Test to generate random Grid
+    def genRandGridTest(self):
+           for y in range (self.column):
+                for x in range (self.row):
+                    _value = random.randrange(0,255,1)
+                    _x = random.randrange(0,self.row,1)
+                    _y = random.randrange(0,self.column,1)
+                    self.setValue(_x,_y,_value)
+
     def getRowLength(self):
         return self.row
 
-    #
     def getColLength(self):
         return self.column
 
@@ -24,6 +49,7 @@ class Grid:
         self.buffer[r * self.column + c] = value
         self.maxSet = False;
         self.avgSet = False;
+
     
     def updateMaxInformation(self):
         maxIndex = self.buffer.index(max(self.buffer)) 
@@ -33,6 +59,18 @@ class Grid:
 
     def getMax(self):
         return max(self.buffer)
+
+    def getMin(self):
+        return min(self.buffer)
+
+    # Converts value to greyscale
+    def getGrey(self, value):
+        # Convert singular value to RGB scale
+        _max = self.getMax()
+        _min = self.getMin()
+      
+       # Convert RGB to grayscale 
+       # return (0.21*R + 0.71*G + 0.07*B)
     
     def getAvg(self) :
         return sum(self.buffer) / len (self.buffer) 
@@ -57,4 +95,3 @@ class Grid:
 
     def gridScalarMultiplication(self, scalar):
         self.buffer = [x*scalar for x in self.buffer]
-

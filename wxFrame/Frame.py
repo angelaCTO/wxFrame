9e328d54cf9b00fa7d91aea58b3614d7d0d6f2cb
Angela To <to.angelac@gmail.com>
2014-02-28 07:21:27 +0100

    Cleaned and added test function calls

--- wxFrame/Frame.py
+++ wxFrame/Frame.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python
-import wx
 from Grid import Grid
 from Box import Box
+import wx
+import time
 
 ''' A simple test driver for Frame/Grid program '''
 
@@ -11,33 +12,28 @@ BOX_WIDTH = 10         # Dimensions of Grid boxes
 BOX_HEIGHT = 10
 
 def main():
-    # Create a new wx python app 
     app = wx.App()
-
-    # Create the sample Frame
     frame = wx.Frame(None, -1, 'TestFrame.py')
 
-    # Testing Grid ... Setting values != 0 paints a 
-    # white box. Still need to figure out gradient coloring
-    # based on grid values.
     grid = Grid(32,31)
-    grid.setValue(0,0,1)
-    grid.setValue(23,18,1)
-    grid.setValue(5,5,1)
-    grid.setValue(2,5,1)
-    grid.setValue(4,8,1)
-    grid.setValue(2,2,1)
+    box = Box(frame, -1, grid, grid.toggleGridTest)
 
-    # Create the colored grid
-    box = Box(frame,-1,grid)
+    box.timer.Start()
+     
+    # Test to toggle alternating Grid boxes 
+    # grid.toggleGridTest()
+
+    # Test to randomly generate Grid 
+    grid.genRandGridTest()
+    
+    # NOTE: still need to implement value -> grayscale conversion
+    
     box.Draw(grid,X_COORD,Y_COORD,BOX_WIDTH,BOX_HEIGHT)
-
-    # Display the sample frame
+     
     frame.Show(True)
-    app.MainLoop();
-
+    app.MainLoop()
+        
 # Start
 if __name__ == '__main__':
-	main()
-
+    main()
 
